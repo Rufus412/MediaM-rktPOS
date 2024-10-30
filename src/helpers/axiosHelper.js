9452fa56b4ac1224a996756455a2f8f466ad8207
jacob <jacob@sannes.se>
2024-10-30 12:52:05 +0100

    print correct response

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -5,7 +5,7 @@ export async function callBackend(path, args){
     let resp = await axios.post(`http://www.sannes.se/api/${path}.php`, new URLSearchParams({ password: args }))
     let a = resp.data
     let b = a.guid
-    console.log(b)
+    console.log(a)
     //let q = await axios.post(`http://www.sannes.se/api/storeGuid.php`, new URLSearchParams({ guid: b }))
     //let v = q.data
     //console.log(v)
