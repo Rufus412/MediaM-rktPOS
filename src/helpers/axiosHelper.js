056d0a9f364d8d5dece04eb17c2e09dda72c4cae
jacob <jacob@sannes.se>
2024-10-31 02:06:06 +0100

    Working code

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export async function postToBackend(path, args){
     let resp = await axios.post(`http://www.sannes.se/api/${path}.php`, new URLSearchParams(args))
     let r = resp.data
-    console.log(r.guid)
+    //console.log(r.guid)
     return(r)
 }   
